3a2fe40af77e3953711e3a32f18d5da49abf3fd3
Sven Rademakers <sven.rademakers@gmail.com>
2021-02-07 12:09:50 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,46 @@
-mod Settings;
 mod app_meta;
 mod logging;
 mod script_engine;
+mod settings;
 
 use app_meta::*;
 use logging::*;
-use script_engine::*;
-use Settings::*;
+use script_engine::{ScriptEngine, ScriptStore};
+use settings::*;
 
 extern crate clap;
-use clap::{App, Arg};
+use clap::{App, AppSettings, Arg, ArgMatches, SubCommand};
+
+fn main() {
+    init_logging(LevelFilter::Debug).unwrap();
+    info!("App:\t{}", APP_INFO.name);
+    info!("Author: \t{}", APP_INFO.author);
+    info!("Version:\t{} ({})", VERSION, BUILD_DATE);
+    info!("--------------------------------------");
+
+    let user_config_path =
+        app_dirs::get_app_root(app_dirs::AppDataType::UserConfig, &app_meta::APP_INFO)
+            .map(|path| {
+                let mut config = path;
+                config.push("config.json");
+                config.to_string_lossy().to_string()
+            })
+            .unwrap();
+
+    let settings = Settings::new(&user_config_path, &setting_defaults());
+    let scripts_path = settings.get_str(SettingKey::ScriptsDir).unwrap();
+    info!("scripts_path: {}", scripts_path);
+
+    let mut script_engine = ScriptEngine::new();
+    script_engine.load(scripts_path).unwrap();
+
+    let matches = get_app_cli(scripts_path);
+    match matches.subcommand_name().unwrap() {
+        "list" => list_subcommand(script_engine.context),
+        "run" => run_subcommand(&matches, script_engine),
+        _ => {}
+    }
+}
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
 pub enum SettingKey {
@@ -39,26 +70,8 @@ fn setting_defaults() -> Vec<KeyWithDefault<SettingKey>> {
     dict
 }
 
-fn main() {
-    init_logging(LevelFilter::Debug).unwrap();
-    info!("App:\t{}", APP_INFO.name);
-    info!("Author: \t{}", APP_INFO.author);
-    info!("Version:\t{} ({})", VERSION, BUILD_DATE);
-    info!("--------------------------------------");
-
-    let user_config_path =
-        app_dirs::get_app_root(app_dirs::AppDataType::UserConfig, &app_meta::APP_INFO)
-            .map(|path| {
-                let mut config = path;
-                config.push("config.json");
-                config.to_string_lossy().to_string()
-            })
-            .unwrap();
-
-    let settings = Settings::Settings::new(&user_config_path, &setting_defaults());
-    let scripts_path = settings.get_str(SettingKey::ScriptsDir).unwrap();
-    info!("scripts_path: {}", scripts_path);
-    let matches = App::new(APP_INFO.name)
+fn get_app_cli(scripts_path: &str) -> ArgMatches {
+    App::new(APP_INFO.name)
         .version(VERSION)
         .author(APP_INFO.author)
         .about(&*format!("run any script def in {}", scripts_path))
@@ -70,26 +83,33 @@ fn main() {
                 .help("Sets a custom config file")
                 .takes_value(true),
         )
-        .arg(
-            Arg::with_name("Script Name")
-                .help("Name of the script to run")
-                .required(true)
-                .index(1),
+        .subcommand(
+            SubCommand::with_name("run")
+                .about("run an arbitrary script")
+                .arg(
+                    Arg::with_name("Script Name")
+                        .help("The configuration file to use")
+                        .index(1)
+                        .required(true),
+                ),
         )
-        .arg(
-            Arg::with_name("v")
-                .short("v")
-                .multiple(true)
-                .help("Sets the level of verbosity"),
-        )
-        .get_matches();
+        .subcommand(SubCommand::with_name("list").about("list all available scripts"))
+        .setting(AppSettings::SubcommandRequired)
+        .get_matches()
+}
 
+fn list_subcommand(script_store: ScriptStore) {
+    for name in script_store.names {
+        println!("\t{}\t\t--\t{}", name.1, script_store.description[name.0]);
+    }
+}
+
+fn run_subcommand(matches: &ArgMatches, script_engine: ScriptEngine) {
     let script_name = matches.value_of("Script Name").unwrap();
-    let mut script_engine = ScriptEngine::new();
-    script_engine.load(scripts_path);
 
+    let arguments = vec![script_engine::Argument::String("BAMI".to_string())];
     if let Some(key) = script_engine.find(script_name) {
-        if script_engine.call(key, &Vec::new()).unwrap() {
+        if script_engine.call(key, &arguments).unwrap() {
             info!("Called {} successfully!", script_name);
         }
     } else {
