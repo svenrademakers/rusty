e1593c0d5aca47d844a29bf0b516e600df8fc655
Sven Rademakers <sven.rademakers@gmail.com>
2021-08-21 12:26:35 +0200

    Enable Github Actions build

--- flaunch_core/src/app_meta.rs
+++ flaunch_core/src/app_meta.rs
@@ -7,6 +7,6 @@ pub const APP_INFO: AppInfo = AppInfo {
 };
 
 pub const APP_NAME: &str = "Flaunch";
-pub const VERSION: &str = "72235b6";
-pub const BUILD_DATE: &str = "7-8-2021";
+pub const VERSION: &str = "28f207b";
+pub const BUILD_DATE: &str = "21-8-2021";
 pub const ICON: &str = "favicon.png";
