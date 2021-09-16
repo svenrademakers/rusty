830394c7c994ae111c451539f378ff12afc00848
Sven Rademakers <sven.rademakers@gmail.com>
2021-09-16 19:44:49 +0200

    Add Menu

--- flaunch_core/src/app_meta.rs
+++ flaunch_core/src/app_meta.rs
@@ -7,6 +7,6 @@ pub const APP_INFO: AppInfo = AppInfo {
 };
 
 pub const APP_NAME: &str = "Flaunch";
-pub const VERSION: &str = "28f207b";
-pub const BUILD_DATE: &str = "21-8-2021";
+pub const VERSION: &str = "e1593c0";
+pub const BUILD_DATE: &str = "16-9-2021";
 pub const ICON: &str = "favicon.png";
